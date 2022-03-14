22f26897b25dec295f466b02fb8e5ea979e4c067
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-14 19:14:07 +0000

    Create home component - without subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -6,9 +7,17 @@ export default function Home() {
       {// tag Head allows to add custom headers per route (page). It supports anything that would go inside the html tag head (canonical tag, meta tags, etc). 
       }
       <Head><title>ig.news Home</title></Head> 
-      <h1>
-        Hello World
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for R$ 9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
