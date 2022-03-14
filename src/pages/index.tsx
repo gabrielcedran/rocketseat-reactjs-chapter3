f199b4430944ecef1ff827801e18699f0215665d
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-14 16:01:51 +0000

    Create application's global css including good practices for a11y

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import styles from '../styles/global.module.scss';
 import Head from 'next/head';
-
-
 
 export default function Home() {
   return (
@@ -9,7 +6,7 @@ export default function Home() {
       {// tag Head allows to add custom headers per route (page). It supports anything that would go inside the html tag head (canonical tag, meta tags, etc). 
       }
       <Head><title>ig.news Home</title></Head> 
-      <h1 className={styles.title}>
+      <h1>
         Hello World
       </h1>
     </>
