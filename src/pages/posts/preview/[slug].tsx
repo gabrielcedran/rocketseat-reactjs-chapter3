d76eee60fdbd939b40d0852289867532309cf357
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-18 18:35:11 +0000

    Configure ttl static page

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -73,6 +73,7 @@ export const getStaticProps: GetStaticProps  = async ({params}) => {
     return {
         props: {
             post
-        }
+        },
+        revalidate: 60 * 30 
     }
 }
